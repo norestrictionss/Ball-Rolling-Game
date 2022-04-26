3391a9405c16f21b03ffa8dc7111103a3403f7e2
Jedsam <fgokgoz02@hotmail.com>
2022-04-26 11:26:37 +0300

    changed add_tiles(i) to a variable to avoid calling the same method multiple times

--- src/CheckPath.java
+++ src/CheckPath.java
@@ -4,172 +4,172 @@ public class CheckPath {
 	public static ArrayList<Tile> generatePath(ArrayList<Tile> all_tiles){
 		ArrayList<Tile> path=new ArrayList<>(0);
 		path.add(all_tiles.get(findStarterIndex(all_tiles)));
-	
+		Tile currentTile;
 		int i=findStarterIndex(all_tiles);
 		while(!(path.get(path.size()-1) instanceof End)) {
-				
-				if(i>=4 && all_tiles.get(i).getProperty().equals("Vertical") && 
+				currentTile = all_tiles.get(i);
+				if(i>=4 && currentTile.getProperty().equals("Vertical") && 
 				   all_tiles.get(i-4).getProperty().equals("Vertical") && 
 				   !isInList(path, all_tiles.get(i-4))){
 					path.add(all_tiles.get(i-4));
 					i-=4;
 				}
-				else if(i<12 && all_tiles.get(i).getProperty().equals("Vertical") && 
+				else if(i<12 && currentTile.getProperty().equals("Vertical") && 
 					all_tiles.get(i+4).getProperty().equals("Vertical") && 
 					!isInList(path, all_tiles.get(i+4))){
 					
 					path.add(all_tiles.get(i+4));
 					i+=4;
 				}
-				else if(i>=4 && all_tiles.get(i).getProperty().equals("Vertical") && 
+				else if(i>=4 && currentTile.getProperty().equals("Vertical") && 
 					all_tiles.get(i-4).getProperty().equals("10") && 
 					!isInList(path, all_tiles.get(i-4))) {
 					path.add(all_tiles.get(i-4));
 					i-=4;
 				}
-				else if(i>=4 && all_tiles.get(i).getProperty().equals("Vertical") && 
+				else if(i>=4 && currentTile.getProperty().equals("Vertical") && 
 					all_tiles.get(i-4).getProperty().equals("11") && 
 					!isInList(path, all_tiles.get(i-4))) {
 					path.add(all_tiles.get(i-4));
 					i-=4;
 				}
-				else if(i<12 && all_tiles.get(i).getProperty().equals("Vertical") && 
+				else if(i<12 && currentTile.getProperty().equals("Vertical") && 
 					all_tiles.get(i+4).getProperty().equals("00") && 
 					!isInList(path, all_tiles.get(i+4))){
 					path.add(all_tiles.get(i+4));
 					i+=4;
 				}
-				else if(i<12 && all_tiles.get(i).getProperty().equals("Vertical") && 
+				else if(i<12 && currentTile.getProperty().equals("Vertical") && 
 					all_tiles.get(i+4).getProperty().equals("01") && 
 					!isInList(path, all_tiles.get(i+4))){
 					path.add(all_tiles.get(i+4));
 					i+=4;
 				}
-				else if(i>=4 && (all_tiles.get(i).getProperty().equals("00")) && 
+				else if(i>=4 && (currentTile.getProperty().equals("00")) && 
 					all_tiles.get(i-4).getProperty().equals("Vertical") && 
 					!isInList(path, all_tiles.get(i-4))) {
 					path.add(all_tiles.get(i-4));
 					i-=4;
 				}
-				else if(i>=1 && (all_tiles.get(i).getProperty().equals("00")) && 
+				else if(i>=1 && (currentTile.getProperty().equals("00")) && 
 					all_tiles.get(i-1).getProperty().equals("Horizontal") && 
 					!isInList(path, all_tiles.get(i-1))) {
 					path.add(all_tiles.get(i-1));
 					i-=1;
 				}
-				else if(i>=4 && (all_tiles.get(i).getProperty().equals("00")) && 
+				else if(i>=4 && (currentTile.getProperty().equals("00")) && 
 						all_tiles.get(i-4).getProperty().equals("10") && 
 						!isInList(path, all_tiles.get(i-4))) {
 						path.add(all_tiles.get(i-4));
 						i-=4;
 				}
-				else if(i>=4 && (all_tiles.get(i).getProperty().equals("00")) && 
+				else if(i>=4 && (currentTile.getProperty().equals("00")) && 
 						all_tiles.get(i-4).getProperty().equals("11") && 
 						!isInList(path, all_tiles.get(i-4))) {
 						path.add(all_tiles.get(i-4));
 						i-=4;
 				}
-				else if(i>=4 && (all_tiles.get(i).getProperty().equals("01")) && 
+				else if(i>=4 && (currentTile.getProperty().equals("01")) && 
 					all_tiles.get(i-4).getProperty().equals("Vertical") && 
 					!isInList(path, all_tiles.get(i-4))) {
 					path.add(all_tiles.get(i-4));
 					i-=4;
 				}
 				
-				else if(i<15 && (all_tiles.get(i).getProperty().equals("01")) && 
+				else if(i<15 && (currentTile.getProperty().equals("01")) && 
 						all_tiles.get(i+1).getProperty().equals("00") && 
 						!isInList(path, all_tiles.get(i+1))) {
 						path.add(all_tiles.get(i+1));
 						i+=1;
 				}
-				else if(i<15 && (all_tiles.get(i).getProperty().equals("01")) && 
+				else if(i<15 && (currentTile.getProperty().equals("01")) && 
 						all_tiles.get(i+1).getProperty().equals("10") && 
 						!isInList(path, all_tiles.get(i+1))) {
 						path.add(all_tiles.get(i+1));
 						i+=1;
 				}
-				else if(i<15 && (all_tiles.get(i).getProperty().equals("01")) && 
+				else if(i<15 && (currentTile.getProperty().equals("01")) && 
 					all_tiles.get(i+1).getProperty().equals("Horizontal") && 
 					!isInList(path, all_tiles.get(i+1))) {
 					path.add(all_tiles.get(i+1));
 					i+=1;
 				}
-				else if(i<12 && (all_tiles.get(i).getProperty().equals("10")) && 
+				else if(i<12 && (currentTile.getProperty().equals("10")) && 
 					all_tiles.get(i+4).getProperty().equals("Vertical") && 
 					!isInList(path, all_tiles.get(i+4))) {
 					path.add(all_tiles.get(i+4));
 					i+=4;
 				}
-				else if(i>1 && (all_tiles.get(i).getProperty().equals("10")) && 
+				else if(i>1 && (currentTile.getProperty().equals("10")) && 
 						all_tiles.get(i-1).getProperty().equals("11") && 
 						!isInList(path, all_tiles.get(i-1))) {
 						path.add(all_tiles.get(i-1));
 						i-=1;
 				}
-				else if(i>1 && (all_tiles.get(i).getProperty().equals("10")) && 
+				else if(i>1 && (currentTile.getProperty().equals("10")) && 
 						all_tiles.get(i-1).getProperty().equals("01") && 
 						!isInList(path, all_tiles.get(i-1))) {
 						path.add(all_tiles.get(i-1));
 						i-=1;
 				}
-				else if(i>1 && (all_tiles.get(i).getProperty().equals("10")) && 
+				else if(i>1 && (currentTile.getProperty().equals("10")) && 
 					all_tiles.get(i-1).getProperty().equals("Horizontal") && 
 					!isInList(path, all_tiles.get(i-1))) {
 					path.add(all_tiles.get(i-1));
 					i-=1;
 				}
-				else if(i<12 && (all_tiles.get(i).getProperty().equals("11")) && 
+				else if(i<12 && (currentTile.getProperty().equals("11")) && 
 					all_tiles.get(i+4).getProperty().equals("Vertical") && 
 					!isInList(path, all_tiles.get(i+4))) {
 					path.add(all_tiles.get(i+4));
 					i+=4;
 				}
-				else if(i<15 && (all_tiles.get(i).getProperty().equals("11")) && 
+				else if(i<15 && (currentTile.getProperty().equals("11")) && 
 						all_tiles.get(i+1).getProperty().equals("10") && 
 						!isInList(path, all_tiles.get(i+1))) {
 						path.add(all_tiles.get(i+1));
 						i+=1;
 				}
-				else if(i<15 && (all_tiles.get(i).getProperty().equals("11")) && 
+				else if(i<15 && (currentTile.getProperty().equals("11")) && 
 						all_tiles.get(i+1).getProperty().equals("00") && 
 						!isInList(path, all_tiles.get(i+1))) {
 						path.add(all_tiles.get(i+1));
 						i+=1;
 				}
-				else if(i<15 && (all_tiles.get(i).getProperty().equals("11")) && 
+				else if(i<15 && (currentTile.getProperty().equals("11")) && 
 					all_tiles.get(i+1).getProperty().equals("Horizontal") && 
 					!isInList(path, all_tiles.get(i+1))) {
 					path.add(all_tiles.get(i+1));
 					i+=1;
 				}
-				else if(i>=1 && (all_tiles.get(i).getProperty().equals("Horizontal")) && 
+				else if(i>=1 && (currentTile.getProperty().equals("Horizontal")) && 
 						all_tiles.get(i-1).getProperty().equals("Horizontal")  && 
 						   !isInList(path, all_tiles.get(i-1))) {
 						path.add(all_tiles.get(i-1));
 						i-=1;
 				}
-				else if(i<15 && (all_tiles.get(i).getProperty().equals("Horizontal")) && 
+				else if(i<15 && (currentTile.getProperty().equals("Horizontal")) && 
 						all_tiles.get(i+1).getProperty().equals("Horizontal") && 
 						   !isInList(path, all_tiles.get(i+1))) {
 						path.add(all_tiles.get(i+1));
 						i+=1;
 				}
-				else if(i<15 && (all_tiles.get(i).getProperty().equals("Horizontal")) && 
+				else if(i<15 && (currentTile.getProperty().equals("Horizontal")) && 
 					all_tiles.get(i+1).getProperty().equals("00")) {
 					path.add(all_tiles.get(i+1));
 					i+=1;
 				}
-				else if(i>1 && (all_tiles.get(i).getProperty().equals("Horizontal")) && 
+				else if(i>1 && (currentTile.getProperty().equals("Horizontal")) && 
 					all_tiles.get(i-1).getProperty().equals("01")) {
 					path.add(all_tiles.get(i-1));
 					i-=1;
 				}
-				else if(i<15 && (all_tiles.get(i).getProperty().equals("Horizontal")) && 
+				else if(i<15 && (currentTile.getProperty().equals("Horizontal")) && 
 					all_tiles.get(i+1).getProperty().equals("10")) {
 					path.add(all_tiles.get(i+1));
 					i+=1;
 				}
-				else if(i>1 && (all_tiles.get(i).getProperty().equals("Horizontal")) && 
+				else if(i>1 && (currentTile.getProperty().equals("Horizontal")) && 
 					all_tiles.get(i-1).getProperty().equals("11")) {
 					path.add(all_tiles.get(i-1));
 					i-=1;
