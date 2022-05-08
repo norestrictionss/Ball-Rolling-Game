4a33480ad5de1bd56d8423ae1db4d015a747823d
norestrictionss <77641983+norestrictionss@users.noreply.github.com>
2022-05-08 20:17:07 +0300

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,17 +1,33 @@
-
-
-
 import javafx.scene.image.ImageView;
-
+/* PROJECT NAME: TILE GRID GAME 
+CONTRIBUTORS
+STUDENT NAME: BARIŞ GİRAY AKMAN -> STUDENT ID: 150121822
+STUDENT NAME: FURKAN GÖKGÖZ-> STUDENT ID: 150120076 
+PURPOSE OF THAT PROGRAM IS SOLVING THE PUZZLE AND HAVING THE BALL MOVED 
+IF THE PUZZLE HAS BEEN SET IN CORRECT COMBINATION.
+*/
 public abstract class Tile {
-	private int id;
-	private String property;
-	private ImageView image;
-	private static int totalMoves;
+	private int id; // ID of tile
+	private String property; // This variable indicates the type of tile
+	private ImageView image; // This variable hold the picture of tile.
+	
+	private static int totalMoves; // This variable hold total move  which was done by the player.
+	private static boolean isAnimationOngoing; /* This variable indicates whether 
+	animation is going or not. */
+	
+	// Parameterized constructor
 	public Tile(int id, String property, ImageView image) {
 		this.setId(id);
 		this.setProperty(property);
 		this.setImage(image);
+	}
+	// Setter-getter methods
+	public static void setIsAnimationOngoing(boolean isAnimationOnGoing) {
+		Tile.isAnimationOngoing = isAnimationOnGoing;
+	}
+	
+	public static boolean getIsAnimationOnGoing() {
+		return isAnimationOngoing;
 	}
 	
 	public static void incrementTotalMoves(){
@@ -49,7 +65,6 @@ public abstract class Tile {
 	public void setImage(ImageView image) {
 		this.image = image;
 	}
-	
 
 	
 }
