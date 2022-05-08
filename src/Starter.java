f99a55cacf2f51dff99dcf6ddb4243af789f8a2f
norestrictionss <77641983+norestrictionss@users.noreply.github.com>
2022-05-08 20:16:39 +0300

    Update Starter.java

--- src/Starter.java
+++ src/Starter.java
@@ -1,8 +1,13 @@
-
-
-
 import javafx.scene.image.ImageView;
-
+/* PROJECT NAME: TILE GRID GAME 
+CONTRIBUTORS
+STUDENT NAME: BARIŞ GİRAY AKMAN -> STUDENT ID: 150121822
+STUDENT NAME: FURKAN GÖKGÖZ-> STUDENT ID: 150120076 
+PURPOSE OF THAT PROGRAM IS SOLVING THE PUZZLE AND HAVING THE BALL MOVED 
+IF THE PUZZLE HAS BEEN SET IN CORRECT COMBINATION.
+*/
+//That class represents Starter tile.
+//That class inherits the features of Tile class.
 public class Starter extends Tile {
 	
 	public Starter(int id, String property, ImageView image) {
