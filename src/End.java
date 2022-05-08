c042c1a7f10106f73ae12b4889d6bc803cf23a36
norestrictionss <77641983+norestrictionss@users.noreply.github.com>
2022-05-08 20:09:46 +0300

    Update End.java

--- src/End.java
+++ src/End.java
@@ -1,8 +1,13 @@
-
-
-
 import javafx.scene.image.ImageView;
-
+/* PROJECT NAME: TILE GRID GAME 
+CONTRIBUTORS
+STUDENT NAME: BARIŞ GİRAY AKMAN -> STUDENT ID: 150121822
+STUDENT NAME: FURKAN GÖKGÖZ-> STUDENT ID: 150120076 
+PURPOSE OF THAT PROGRAM IS SOLVING THE PUZZLE AND HAVING THE BALL MOVED 
+IF THE PUZZLE HAS BEEN SET IN CORRECT COMBINATION.
+*/
+// That class represents end tile.
+// That class inherits the features of Tile class.
 public class End extends Tile {
 	public End(int id, String property, ImageView image) {
 		super(id, property, image);
