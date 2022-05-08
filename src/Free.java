4708f107cecba603ffc0ada318b5dba3bcd1d3fd
norestrictionss <77641983+norestrictionss@users.noreply.github.com>
2022-05-08 20:11:26 +0300

    Update Free.java

--- src/Free.java
+++ src/Free.java
@@ -1,8 +1,13 @@
-
-
-
 import javafx.scene.image.ImageView;
-
+/* PROJECT NAME: TILE GRID GAME 
+CONTRIBUTORS
+STUDENT NAME: BARIŞ GİRAY AKMAN -> STUDENT ID: 150121822
+STUDENT NAME: FURKAN GÖKGÖZ-> STUDENT ID: 150120076 
+PURPOSE OF THAT PROGRAM IS SOLVING THE PUZZLE AND HAVING THE BALL MOVED 
+IF THE PUZZLE HAS BEEN SET IN CORRECT COMBINATION.
+*/
+//That class represents the Free tile.
+//That class inherits the features of the Tile class.
 public class Free extends Tile {
 	public Free(int id, String property, ImageView image) {
 		super(id, property, image);
